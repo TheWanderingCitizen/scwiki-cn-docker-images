35d9e45c3a040fcfc63219785ab07267051c87f3
alistair3149 <alistair3149@users.noreply.github.com>
2024-02-07 15:40:28 -0500

    Prepare to switch Module:DependencyList to SMW

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -443,13 +443,65 @@ $smwgEnabledQueryDependencyLinksStore = true;
 # Duplicate query conditions should be removed from computing query results
 $smwgQFilterDuplicates = true;
 $smwgConfigFileDir = "/usr/local/smw";
+# Enable SMW in the following namespaces
+# Template namespace
+$smwgNamespacesWithSemanticLinks[NS_TEMPLATE] = true;
+# Module namespace
+$smwgNamespacesWithSemanticLinks[828] = true;
 
 # Semantic Extra Special Properties
 $sespgUseFixedTables = true;
+# Required by Module:DependencyList
+$sespgLocalDefinitions['_LINKSTO'] = [
+    'id'    => '_LINKSTO',
+    'type'  => '_wpg',
+    'alias' => 'sesp-property-links-to',
+    'desc' => 'sesp-property-links-to-desc',
+    'label' => 'Links to',
+    'callback'  => static function(\SESP\AppFactory $appFactory, \SMW\DIProperty $property, \SMW\SemanticData $semanticData ) {
+        $page = $semanticData->getSubject()->getTitle();
+
+        // The namespaces where the property will be added
+        $targetNS = [ 10, 828 ];
+
+        if ( $page === null || !in_array( $page->getNamespace(), $targetNS, true ) ) {
+            return;
+        }
+
+        /** @var \Wikimedia\Rdbms\DBConnRef $con */
+        $con = $appFactory->getConnection();
+
+        $where = [];
+        $where[] = sprintf('pl.pl_from = %s', $page->getArticleID() );
+        $where[] = sprintf('pl.pl_title != %s', $con->addQuotes( $page->getDBkey() ) );
+
+        if ( !empty( $targetNS ) ) {
+            $where[] = sprintf( 'pl.pl_namespace IN (%s)', implode(',', $targetNS ) );
+        }
+
+        $res = $con->select(
+            [ 'pl' => 'pagelinks', 'page' ],
+            [ 'sel_title' => 'pl.pl_title', 'sel_ns' => 'pl.pl_namespace' ],
+            $where,
+            __METHOD__,
+            [ 'DISTINCT' ],
+            [ 'page' => [ 'JOIN', 'page_id=pl_from' ] ]
+        );
+
+        foreach( $res as $row ) {
+            $title = Title::newFromText( $row->sel_title, $row->sel_ns );
+            if ( $title !== null && $title->exists() ) {
+                $semanticData->addPropertyObjectValue( $property,\SMW\DIWikiPage::newFromTitle( $title ) );
+            }
+        }
+    }
+];
+
 $sespgEnabledPropertyList = [
 	'_USERREG',
 	'_USEREDITCNT',
-	'_PAGEIMG'
+	'_PAGEIMG',
+	'_LINKSTO'
 ];
 
 # Scribunto
