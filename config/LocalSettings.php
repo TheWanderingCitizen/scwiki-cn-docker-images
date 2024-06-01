7d2e5cc8d6bb92edcf574b34f7b421bb6eda7d1f
alistair3149 <alistair31494322@gmail.com>
2024-06-01 17:13:52 -0400

    Revert "Set $smwgCheckForConstraintErrors instead of user pref"
    This reverts commit 167ba8212cd10bdd73e6f4f5091c9a00b6ee8fff.

--- config/LocalSettings.php
+++ config/LocalSettings.php
@@ -468,9 +468,9 @@ $smwgConfigFileDir = "/usr/local/smw";
 $smwgNamespacesWithSemanticLinks[NS_TEMPLATE] = true;
 # Module namespace
 $smwgNamespacesWithSemanticLinks[828] = true;
-# Disable entity examiner
+# Disable entity issue panel for all users by default since it is useless to most users
 # This generates an uncached call to api.php which is not needed
-$smwgCheckForConstraintErrors = SMW_CONSTRAINT_ERR_CHECK_NONE;
+$wgDefaultUserOptions['smw-prefs-general-options-show-entity-issue-panel'] = false;
 
 # Semantic Extra Special Properties
 $sespgUseFixedTables = true;
